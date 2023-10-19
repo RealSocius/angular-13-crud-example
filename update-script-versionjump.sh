7374cfcce9d13c2d152d00f4cad8adc428ef66da
sebastian@hauschildt.dev <sebastian@hauschildt.dev>
2023-10-19 23:55:57 +0200

    Make versionjump script work

--- update-script-versionjump.sh
+++ update-script-versionjump.sh
@@ -1,6 +1,5 @@
 pnpm update --latest
 pnpm i -D typescript@5.1
-# Error 1
-# https://stackoverflow.com/questions/56644864/uncaught-typeerror-webpack-require-context-is-not-a-function
-sed -i 's|"main": "src/test.ts",||' angular.json
-sed -i 's|"polyfills": "src/polyfills.ts",|"polyfills": ["zone.js", "zone.js/testing"],|' angular.json
+
+# Update test.ts to make it work -> copied behaviour from ng update
+sed -i -e '10,16d;22,27d' src/test.ts
