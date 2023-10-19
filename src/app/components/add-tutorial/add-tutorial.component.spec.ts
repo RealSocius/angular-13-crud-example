9053b19829a9921b684619f0d13ddb01de6c16a1
sebastian@hauschildt.dev <sebastian@hauschildt.dev>
2023-10-19 23:40:15 +0200

    fix one test

--- src/app/components/add-tutorial/add-tutorial.component.spec.ts
+++ src/app/components/add-tutorial/add-tutorial.component.spec.ts
@@ -55,7 +55,6 @@ describe('AddTutorialComponent', () => {
     expect(tutorialService.create).toHaveBeenCalledWith({
       title: 'Test Title',
       description: 'Test Description',
-      published: true,
     });
   });
 
